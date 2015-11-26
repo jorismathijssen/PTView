073f757f88accdc3db123451d8c4c17b52e3e08e
loljimmo12 <spongebob.958@gmail.com>
2015-11-26 14:53:25 +0100

    Adding ipcproto.h

---
+++ ipcproto.h
@@ -0,0 +1,16 @@
+/* 
+IPC protocol definiton for the the PTMain system and the QT touchscreen application
+*/
+#ifndef IPCPROTO_H
+#define IPCPROTO_H
+
+#define PT_HANDSHAKE 0
+#define PT_REQUEST_BATTERY_INFO 1
+#define PT_REQUEST_SPEED 2
+#define PT_REQUEST_ALL 3
+
+#define PT_ACK 10
+#define PT_NACK 11
+#define PT_RESPONSE 12 //note: A response is followed by data and data is then ended with a 0 byte
+
+#endif
